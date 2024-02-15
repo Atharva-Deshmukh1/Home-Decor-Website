f7d58518890d91eed6431b5b27815863f9b542f6
Atharva-Deshmukh1 <atharva25deshmukh@gmail.com>
2024-02-15 23:34:46 +0530

    u

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import ProductDisplay from '../ProductDisplay/ProductDisplay';
-import Item from '../Item/Item'
-import All_product_data from '../Assets/AllProduct';
 import Homescreen from '../Homescreen/Homescreen';
 
 export default function ProductPage() {
   return (
-    // <div className="dark:bg-gray-800">
-
-
-    //   <div className='flex flex-wrap justify-center gap-14 mt-10'>
-    //     {All_product_data.map((item, i) => {
-    //       return <Item key={i} id={item.id} name={item.Title} num={item.Price} url={item.url} />
-    //     })}
-    //   </div>
-    // </div>
+    
     <>
     <Homescreen />
     </>
